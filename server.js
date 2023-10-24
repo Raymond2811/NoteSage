2d13c4d58ee0e944b090819d6a932714ce93cd68
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-10-24 01:13:24 -0700

    added express server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+require('./routes/htmlRoutes/index')(app);
+
+app.listen(PORT, () => console.log (`Server  started at http://localhost:${PORT}`));
