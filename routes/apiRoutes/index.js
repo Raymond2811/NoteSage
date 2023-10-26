f4dcd952640cefc5da922eefe373e3ef20a649c1
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-10-25 18:32:45 -0700

    added note reading route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,20 @@ const router = require('express').Router();
 const path = require('path');
 const { v4: uuid } = require('uuid');
 
+const db = path.join(__dirname, '../../db/db.json');
 
+router.get('/api/notes', (req, res) => {
+    fs.readFile(db, (err, data) => {
+      if (err) {
+        return res.status(500).send('Error reading the file.');
+      }
+      try {
+        const parsedData = JSON.parse(data);
+        res.json(parsedData);
+      } catch (parseError) {
+        res.status(500).send('Error parsing JSON data.');
+      }
+    });
+});
 
 module.exports = router;
